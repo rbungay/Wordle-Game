8dcfeb9c51499ba37264548da26de6bae1f153e2
Rani Bungay <ranibungay@gmail.com>
2024-09-14 11:19:10 -0400

    understood pathing of updatig board

--- js/main.js
+++ js/main.js
@@ -23,7 +23,7 @@ const sqrEl = document.querySelectorAll(".sqr");
 
 const updateBoard = (char) => {
   console.log("This is the actual updateBoard function");
-  console.log(char);
+  console.log();
 };
 
 const handleKey = (event) => {
@@ -32,26 +32,3 @@ const handleKey = (event) => {
 };
 
 window.addEventListener("keydown", handleKey);
-
-// window.onload = () => {
-//   init();
-// };
-
-/*
-const updateBoard = (char) =>{
-    for (let i = 0; i<board.length; i++){
-    if (board[i] === ""){
-    board[i] === char
-    break
-    }
-}}
-
-let currntboard = boardOneEl to keep track of the id with the current board
-const render = () => {
-board.forEach(spot, index) => {
-    boarrdOneEl.children[index].innerText = board[index]}
-    }
-
-init is suppose to set something up
-
-render is suppose to show you what you need to know. */
