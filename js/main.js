c9bec71c68007eba59b2b9a2388d3ce24b628972
Rani Bungay <ranibungay@gmail.com>
2024-09-14 11:03:27 -0400

    passing the different functions to be able to manipulate info

--- js/main.js
+++ js/main.js
@@ -1,13 +1,57 @@
 /*-------------------------------- Constants --------------------------------*/
 
-import { fiveLetterWords } from "./data.js";
-
-console.log(fiveLetterWords);
+import { fiveLetterWords, letters } from "./data.js";
 
 /*---------------------------- Variables (state) ----------------------------*/
 
+let board = ["", "", "", "", ""];
+let winner = false;
 /*------------------------ Cached Element References ------------------------*/
+
+const boardOneEl = document.querySelector(".boardOne");
+const boardTwoEl = document.querySelector(".boardTwo");
+const boardThreeEl = document.querySelector(".boardThree");
+const boardFourEl = document.querySelector(".boardFour");
+const boardFiveEl = document.querySelector(".boardFive");
+const boardSixEl = document.querySelector(".boardSix");
+const fullBoard = document.querySelector(".board");
+const sqrEl = document.querySelectorAll(".sqr");
+
+/*-------------------------------- Functions --------------------------------*/
 
 /*----------------------------- Event Listeners -----------------------------*/
 
-console.log("this is printing");
+const updateBoard = (char) => {
+  console.log("This is the actual updateBoard function");
+  console.log(char);
+};
+
+const handleKey = (event) => {
+  console.log("this is where i'll be passing the letter", event.key);
+  updateBoard(event.key);
+};
+
+window.addEventListener("keydown", handleKey);
+
+// window.onload = () => {
+//   init();
+// };
+
+/*
+const updateBoard = (char) =>{
+    for (let i = 0; i<board.length; i++){
+    if (board[i] === ""){
+    board[i] === char
+    break
+    }
+}}
+
+let currntboard = boardOneEl to keep track of the id with the current board
+const render = () => {
+board.forEach(spot, index) => {
+    boarrdOneEl.children[index].innerText = board[index]}
+    }
+
+init is suppose to set something up
+
+render is suppose to show you what you need to know. */
---
+++ sandbox.js
@@ -0,0 +1,53 @@
+// const updateBoard = (char) => {
+//   board.forEach((square, index) => {
+//     if (square === "") {
+//       board[index] = char;
+//     }
+//   });
+// };
+
+// const updateDisplay = board.forEach((square, index) => {
+//   sqrEl[index].innerText = square;
+// });
+
+// const init = () => {
+//   let winner = false;
+//   let board = ["", "", "", "", ""];
+//   render();
+// };
+
+// const handleKey = (event) => {
+//   updateBoard(event.key);
+//   console.log(updateBoard());
+//   console.log(event.key);
+
+// const render = () => {
+//   board.map((location, index) => {
+//     boardOneEl.children[index].innerText = board[index];
+//   });
+//   //   placeLetter();
+//   updateBoard();
+// };
+
+// window.onload = () => {
+//   init();
+// };
+
+/*
+const updateBoard = (char) =>{
+    for (let i = 0; i<board.length; i++){
+    if (board[i] === ""){
+    board[i] === char
+    break
+    }
+}}
+
+let currntboard = boardOneEl to keep track of the id with the current board
+const render = () => {
+board.forEach(spot, index) => {
+    boarrdOneEl.children[index].innerText = board[index]}
+    }
+
+init is suppose to set something up
+
+render is suppose to show you what you need to know. */
